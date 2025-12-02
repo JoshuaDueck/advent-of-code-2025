eb0558e658bc1bac21536673610b963abcff13b0
Joshua <joshua.dueck@gmail.com>
2025-12-02 07:19:04 -0600

    BRUH FINALLY CAUGHT THE RIGHT EDGE CASES UGH

--- day1/day.py
+++ day1/day.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import pdb
 
 
 def main():
@@ -51,33 +52,63 @@ def part1(lines):
     return num_zeroes
 
 
-# Doesn't account for rotations > 100 positions.
-# Will need to get the number, negative or positive
-# Doing value % 100 gives us the correct value
-# The size of the value pre-mod will give us the number of times crossing 0 (?)
+# We're counting cases where it STARTS at 0, I think? Or rather, cases where
+# it starts at 0 and goes negative, or starts at 99 and goes positive...
+# We're also missing cases where it starts positive and goes directly to 0?
+# Like, if it's 0, 0 // 100 = 0, despite the fact we're pointing at 0.
 # 6291 < x < 6408 (not 6354...)
 def part2(lines):
     dial_value = 50
     num_zeroes = 0
 
     for line in lines:
-        dial_value, nzc = update_dial(dial_value, line[0], int(line[1:]))
-        num_zeroes += nzc
+        dial_value, nzc = update_dial2(
+            dial_value, line[0], int(line[1:]))
+
+        num_zeroes += nzc if nzc > 0 else 0
 
     return num_zeroes
 
 
-def update_dial(dial, direction, value):
+def update_dial(curr_dial, direction, value):
     polarity = 1
     if direction == 'L':
         polarity = -1
-    divisor = 100
-    dial += value*polarity
-    if dial < 0:
-        divisor = 99
-    nzc = abs(dial // divisor)
-    dial %= divisor
-    return (dial, nzc)
+
+    full_dial = curr_dial+(value*polarity)
+    num_times_past_z = abs(full_dial // 100)
+
+    if full_dial == 0:
+        num_times_past_z += 1
+
+    if curr_dial == 0:
+        num_times_past_z -= 1
+
+    curr_dial = full_dial % 100
+    return (curr_dial, num_times_past_z)
+
+
+# The issue is that we're counting when it starts at 0 and moving left, and failing to count if it ends at 0 (after a single turn, I think?).
+def update_dial2(curr_dial, direction, value):
+    polarity = 1
+    if direction == 'L':
+        polarity = -1
+
+    full_dial = curr_dial+(value*polarity)
+    num_times_past_z = abs(full_dial // 100)
+
+    if direction == 'L':
+        if curr_dial == 0:
+            num_times_past_z -= 1
+
+    curr_dial = full_dial % 100
+
+    if curr_dial == 0 and direction == 'L':
+        num_times_past_z += 1
+
+    print(f'The dial is rotated {direction}{value} to point at {
+          curr_dial}{f'; during this rotation, it points at 0 {f'{num_times_past_z} times' if num_times_past_z > 1 else 'once'}.' if num_times_past_z > 0 else '.'}')
+    return (curr_dial, num_times_past_z)
 
 
 if __name__ == '__main__':
